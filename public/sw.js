f3f4fc87b0d9b3def9b7d25b53acb8359a520b72
fumiya-tech-lab <fumiya1122@gmail.com>
2025-12-31 16:04:19 +0900

    Add notification click event listener
    Define behavior for notification click events.

--- public/sw.js
+++ public/sw.js
@@ -98,3 +98,14 @@ define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
   }), 'GET');
 
 }));
+
+// 通知がクリックされた時の挙動を定義
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return clients.openWindow('/');
+    })
+  );
+});
